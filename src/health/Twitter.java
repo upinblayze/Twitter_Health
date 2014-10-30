71c14589df4e3ee841ada80a364977d4c1c9599c
upinblayze <zhenghannin@gmail.com>
2014-10-30 11:36:06 -0700

    First commit

---
+++ src/health/Twitter.java
@@ -0,0 +1,14 @@
+package health;
+
+public class Twitter {
+
+	public Twitter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
